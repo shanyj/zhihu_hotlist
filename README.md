8c2d6ce4e56617b9cb8538e970220d00b072163b
shanyunji <shanyunji@taou.com>
2024-05-10 17:16:03 +0800

    热榜 相似问题 和回答

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # zhihu_hotlist
+
+### 获取登录token的方式
+如果提示需要手机登录，则电脑上打开知乎网页，按F12然后在调试窗口的console页中输入下方代码并回车，返回 undefined 即为成功。
+
+window.addEventListener("beforeunload", function() { debugger; }, false)
+
+接着打开调试窗口的network页，点击clear按钮清除所有记录。
+
+接着在网页上用手机登录，登录后会触发调试窗口暂停，在 network 页中找到 sign_in 条目，
+选中后从 response 中把内容复制出来，粘贴到 config.json 的 token_json 中覆盖原条目。
+
+这个请求的body是加密的，暂时没找到自动发请求的方法，得手动复制。
+
+
+```python
+
+from zhihu_oauth import ZhihuClient, Answer
+from zhihu_oauth.oauth.token import ZhihuToken
+
+ZhihuToken.from_dict(zhihu_config['token_json']).save(zhihu_config['token_file'])
+
+client = ZhihuClient()
+
+if os.path.isfile(zhihu_config['token_file']):
+    client.load_token(zhihu_config['token_file'])
+
+
+# token_json 长这样
+
+{
+      "user_id": 1111111111111111111,
+      "uid": "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+      "access_token": "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+      "lock_in": 1800,
+      "expires_in": 15000000,
+      "token_type": "bearer",
+      "cookie": {
+        "q_c0": "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+        "z_c0": "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+      },
+      "unlock_ticket": "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+      "refresh_token": "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+}
--- zhihu_crawler.py
+++ zhihu_crawler.py
@@ -1,29 +1,91 @@
 import datetime
 import requests
+from zhihu_oauth import ZhihuClient
+from zhihu_oauth.oauth.token import ZhihuToken
+from zhihu_oauth.zhcls.question import Question
 
 url = "https://www.zhihu.com/api/v3/feed/topstory/hot-lists/total?limit=50&desktop=true"
 headers = {
-        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36",
+    "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36",
+}
+
+zhihu_token_config = {
+    "uid": "f5494f977cf33ec5fe29764f4f8b993f",
+    "user_id": 667301141109542912,
+    "access_token": "2.1dz5xAgAAAAAA4F75HulNEyYAAABgAlVNaSQrZwBZFMROLPAdX5LzGkb2C2XtzsGGJw",
+    "token_type": "bearer",
+    "refresh_token": "2.1283516d52441841c37acf2d3",
+    "expires_in": 15552000,
+    "cookie": {
+        "q_c0": "2|1:0|10:1715328617|4:q_c0|92:Mi4xZHo1eEFnQUFBQUFBNEY3NUh1bE5FeVlBQUFCZ0FsVk5hU1FyWndCWkZNUk9MUEFkWDVMekdrYjJDMlh0enNHR0p3|e0cde53ba114a2ccb227e621c630f3ec20e6dfeb395242bf99eb8c97ee5ffb55",
+        "z_c0": "2|1:0|10:1715328617|4:z_c0|92:Mi4xZHo1eEFnQUFBQUFBNEY3NUh1bE5FeVlBQUFCZ0FsVk5hU1FyWndCWkZNUk9MUEFkWDVMekdrYjJDMlh0enNHR0p3|971ff8ad2de1423c7c6ad1da1efabca616f4436d7ca101f8a1732dc8f35372d6"
+    },
+    "unlock_ticket": "ABAMMtq6QgkmAAAAYAJVTXHdPWZIG-n6YABnf4oqmNlX3R-YnUElGg==",
+    "lock_in": 1800
 }
 
 
-now_time = datetime.datetime.now()
-year = now_time.year
-month = now_time.month
-day = now_time.day
-hour = now_time.hour
+def login():
+    client = ZhihuClient()
+    ZhihuToken.from_dict(zhihu_token_config).save('token.pkl')
+    client.load_token('token.pkl')
+    if client.is_login:
+        return client
+    return None
 
 
-sess = requests.Session()
-res = sess.get(url, headers=headers)
-data = res.json()["data"]
-#print(data)
-hot_list = []
-for item in data:
-    item_id = item["target"]["id"]
-    item_title = item["target"]["title"]
-    hot_list.append("{}: {}".format(item_id, item_title))
+def get_hot_list():
+    now_time = datetime.datetime.now()
+    year = now_time.year
+    month = now_time.month
+    day = now_time.day
+    hour = now_time.hour
 
-output = "\n".join(hot_list)
-with open("./hotlist/{}_{}_{}_{}.txt".format(year, month, day, hour), mode="w") as f:
-    f.write(output)
+    sess = requests.Session()
+    res = sess.get(url, headers=headers)
+    data = res.json()["data"]
+    hot_list = []
+    for item in data:
+        item_id = item["target"]["id"]
+        item_title = item["target"]["title"]
+        hot_list.append("{}: {}".format(item_id, item_title))
+
+    output = "\n".join(hot_list)
+    with open("./hotlist/{}_{}_{}_{}.txt".format(year, month, day, hour), mode="w") as f:
+        f.write(output)
+
+
+def get_similiar_question(question_id):
+    url = "https://www.zhihu.com/api/v4/questions/{}/similar-questions?limit=2".format(question_id)
+    sess = requests.Session()
+    res = sess.get(url, headers=headers)
+    data = res.json()["data"]
+    similiar_questions = []
+    for item in data:
+        item_id = item["id"]
+        item_title = item["title"]
+        similiar_questions.append("{}: {}".format(item_id, item_title))
+
+    output = "\n".join(similiar_questions)
+    with open("./similiar_questions/{}.txt".format(question_id), mode="w") as f:
+        f.write(output)
+
+
+def get_hot_answer(client, question_id):
+    question = client.question(question_id)
+    # for _, answer in zip(range(5), question.answers.order_by('default')):
+    answers = []
+    for _, answer in zip(range(50), question.answers):
+        answers.append(answer)
+    answers = sorted(answers, key=lambda x: x.voteup_count, reverse=True)
+    return answers[:5]
+
+
+if __name__ == "__main__":
+    client = login()
+    if not client:
+        print("Login failed")
+        exit(1)
+    answers = get_hot_answer(client, 655536394)
+    for a in answers:
+        print(a.voteup_count, a.author.name, a.content)
